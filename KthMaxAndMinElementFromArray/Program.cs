355aefdf6bd694fd5a579a2b6e8df29249782c3d
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2021-10-10 12:53:35 +0530

    kth Max and Min element of Array - pending

---
+++ KthMaxAndMinElementFromArray/Program.cs
@@ -0,0 +1,28 @@
+﻿// Find kth Minimum element from array.
+
+
+int[] arr = new int[] { 7, 10, 4, 3, 20, 15 };
+
+int k = 3;
+
+int MIN = arr[0];
+
+int count = 0;
+
+for(int i = 0; i < arr.Length; i++)
+{
+    if(arr[i] < MIN)
+    {
+        MIN = arr[i];
+        count = count + 1;
+
+        if(count == k)
+        {
+            break;
+        }
+    }
+}
+
+Console.WriteLine("Kth minmum element - " + MIN);
+
+Console.ReadLine();
