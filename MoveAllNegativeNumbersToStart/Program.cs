d639594e373fed8f57bc37649334d1c25be3cc1a
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2021-10-10 15:41:33 +0530

    Move all negative numbers to start

---
+++ MoveAllNegativeNumbersToStart/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int[] arr = new int[] { -12, 11, -13, -5, 6, -7, 5, -3, -6 };
+
+int j = 0;
+
+var temp = 0;
+
+for(int i = 0; i < arr.Length; i++)
+{
+    if(arr[i] < 0)
+    {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        j++;
+    }
+}
+
+foreach (var item in arr)
+    Console.Write(item + " ");
+
+
+Console.ReadLine();
