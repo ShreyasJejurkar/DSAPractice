d3295315acf1d7ae7d0f762318bb0fbb063cac32
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2021-10-10 11:52:39 +0530

    Add project files.

---
+++ ReverseAnArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// Reverse an array
+
+int[] arr = new int[] { 1, 2, 3 };
+
+List<int> result = new List<int>();
+
+for(int i = arr.Length - 1; i >= 0; i--)
+{
+    result.Add(arr[i]);
+}
+
+foreach(var item in result)
+    Console.Write(item + " ");
+
+Console.ReadLine();
+
+
+// Result = [3,2,1]
+// Time Complexity = O(n)
