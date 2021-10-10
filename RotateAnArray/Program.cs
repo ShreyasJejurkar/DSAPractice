2755071bfe41656085b38e0f7a159b4d6624b808
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2021-10-10 16:11:49 +0530

    Rotate an array

---
+++ RotateAnArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// Rotate an array by one position
+
+
+int[] arr = new int[] { 1, 2, 3, 4, 5 };
+
+var last = arr[arr.Length - 1];
+
+for (int i = arr.Length - 1; i > 0; i--)
+{
+    arr[i] = arr[i-1];
+}
+
+arr[0] = last;
+
+foreach (var item in arr)
+    Console.Write(item + " ");
+
+
+Console.ReadLine();
